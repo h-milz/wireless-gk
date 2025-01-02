50372ee73679724891e29c3c935ff159c43bde09
Harald Milz <hm@seneca.muc.de>
2025-01-02 09:54:54 +0100

    initial changes

--- main/main.c
+++ main/main.c
@@ -28,7 +28,7 @@
 #define NSAMPLES                60                      // the number of samples we want to send in a batch
 #define NUM_SLOTS               4                       // TDM256, 8 slots per sample
 #define SLOT_WIDTH              32                      // 
-#define DMA_BUFFER_COUNT        8                       // Number of DMA buffers
+#define DMA_BUFFER_COUNT        4                       // Number of DMA buffers
 #define DMA_BUFFER_SIZE         NSAMPLES * NUM_SLOTS * SLOT_WIDTH / 8  // Size of each DMA buffer
 
 #define SAMPLE_RATE             44100                   // 44100
@@ -45,19 +45,15 @@
 
 static const char *TAG = "wirelessGK";
 
-// static uint32_t end_time = 0, elapsed_time = 0;
-static bool sender = false;
-// static volatile bool i2s_data_ready = false;
+// static volatile uint32_t sample_count = 0; // , txcount = 0, rxcount = 0, losses = 0, loopcount = 0, overall_losses = 0, overall_packets = 0;
 
-static volatile uint32_t sample_count = 0; // , txcount = 0, rxcount = 0, losses = 0, loopcount = 0, overall_losses = 0, overall_packets = 0;
-
-static uint8_t i2sbuf[2 * NUM_SLOTS * NSAMPLES];
+static uint8_t i2sbuf[4 * NUM_SLOTS * NSAMPLES];
 static uint8_t udpbuf[3 * NUM_SLOTS * NSAMPLES];
 
 static i2s_chan_handle_t rx_handle = NULL;
 // static i2s_chan_handle_t tx_handle = NULL;
 
-// static TaskHandle_t rx_task_handle; 
+static TaskHandle_t udp_send_task_handle; 
 
 // The channel config is the same for both. 
 // static i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
@@ -78,14 +74,6 @@ static i2s_tdm_config_t rx_cfg = {
     },
     .slot_cfg = I2S_TDM_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_MONO,
                                                 I2S_TDM_SLOT0 | I2S_TDM_SLOT1 | I2S_TDM_SLOT2 | I2S_TDM_SLOT3),
-/*    
-              {
-        .data_bit_width = SLOT_WIDTH,
-        .slot_bit_width = SLOT_WIDTH,
-        .slot_mode = I2S_SLOT_MODE_STEREO,
-        .slot_mask = I2S_TDM_SLOT0 | I2S_TDM_SLOT1,
-    },
-*/    
     .gpio_cfg = {
         .mclk = GPIO_NUM_3,
         .bclk = GPIO_NUM_4,
@@ -128,8 +116,11 @@ volatile int p = 0;
 
 #define NUM 200
 typedef struct { 
-    uint8_t loc;
-    uint32_t time;
+    uint8_t loc;        // location
+    uint32_t time;      // timestamp in µs
+    uint8_t *ptr;       // pointer to buffer
+    uint8_t *ptr2;       // pointer to buffer
+    size_t size;        // data size
 } log_t; 
 
 volatile log_t _log[NUM];   
@@ -160,23 +151,98 @@ static uint32_t get_time_us_in_isr(void) {
     return (uint32_t) timer_val & 0xffffffff;
 }
 
+
+static IRAM_ATTR bool i2s_rx_callback(i2s_chan_handle_t handle, i2s_event_data_t *event, void *user_ctx) {
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    // gpio_set_level(SIG2_PIN, 1);
+    // gpio_set_level(SIG2_PIN, 0);
+    _log[p].loc = 1;
+    _log[p].time = get_time_us_in_isr();
+    _log[p].ptr = event->dma_buf;
+    _log[p].size = event->size; 
+    p++;
+    // TODO either pass the dma_buf as (uint32_t), or we do the packing here and send the index of the udp_buf. 
+    xTaskNotifyFromISR(udp_send_task_handle, (uint32_t)_log[p].ptr, eSetValueWithOverwrite, &xHigherPriorityTaskWoken);
+    return (xHigherPriorityTaskWoken == pdTRUE);
+}    
+
+
+static void udp_tx_task(void *args) {
+    size_t size;
+    uint8_t *dma_buf;
+    uint8_t *ptr;
+    uint32_t ptrval;
+    char t[][15] = {"", "ISR", "begin loop", "channel_read", "packing", "udp send", "end loop" }; 
+    
+    // we get passed the *dma_buf and size, then pack, optionally checksum, and send. 
+    while(1) {
+        xTaskNotifyWait( 0, 
+                         ULONG_MAX,
+                         &ptrval,
+                         portMAX_DELAY);
+        // pass the dma ptr as uint32_t
+        ptr = (uint8_t *)ptrval;
+        // fetch the values from 1 tick earlier
+        dma_buf = _log[p-1].ptr;
+        size = (size_t)_log[p-1].size; 
+        // we're there. 
+        _log[p].loc = 5;
+        _log[p].time = get_time_us_in_isr();
+        _log[p].ptr = ptr;
+        _log[p].ptr2 = dma_buf;
+        _log[p].size = size;
+        p++;
+        // packing
+        
+        // checksumming
+        
+        // send. 
+    
+        if (p > NUM) {
+            int q; 
+            for (q=0; q<p; q++) {
+                switch (_log[q].loc) {
+                    case 1:         // ISR
+                        printf ("%15s time %lu dma_buf 0x%08x size %lu \n", t[_log[q].loc], _log[q].time, _log[q].ptr, _log[q].size);
+                        break; 
+                    case 5:         // UDP task
+                        printf ("%15s time %lu diff %lu ptr 0x%08x dma_buf 0x%08x size %lu \n", 
+                                t[_log[q].loc], _log[q].time, _log[q].time-_log[q-1].time, _log[q].ptr, _log[q].ptr2, _log[q].size);
+                        break;
+                    default:
+                        break;                        
+                
+                }                        
+            }
+            vTaskDelete(NULL); 
+        }
+    
+        
+    
+    }    
+
+}
+
+
+
 void app_main(void) {
     // int i, n;
     // int64_t start_time, stop_time;
     // float elapsed_time;
+    bool setup = false, sender = false; 
     
     ESP_ERROR_CHECK(nvs_flash_init());
     
     // check if we're sender or receiver. The sender has ID_PIN = 0
     gpio_reset_pin(ID_PIN);
     gpio_set_direction(ID_PIN, GPIO_MODE_INPUT);
-    // ESP_ERROR_CHECK(gpio_input_enable(ID_PIN)); 
     gpio_set_pull_mode(ID_PIN, GPIO_PULLUP_ONLY);
     gpio_pullup_en(ID_PIN);
     if (gpio_get_level(ID_PIN) == 0) {
         sender = true; 
     } 
-    
+    gpio_reset_pin(ID_PIN);
+        
     // signal pin for loop measurements
     gpio_reset_pin(SIG_PIN);
     gpio_set_direction(SIG_PIN, GPIO_MODE_OUTPUT);
@@ -189,19 +255,22 @@ void app_main(void) {
     gpio_set_direction(SIG2_PIN, GPIO_MODE_OUTPUT);
     gpio_pullup_dis(SIG2_PIN);
     gpio_set_intr_type(SIG2_PIN, GPIO_INTR_DISABLE);
-    gpio_set_level(SIG2_PIN, 0);
-    
+    gpio_set_level(SIG2_PIN, 0);    
 
-    // TODO check if SETUP is pressed and then enter setup mode.  
+    // check if SETUP is pressed and then enter setup mode.  
+    gpio_reset_pin(SETUP_PIN);
+    gpio_set_direction(SETUP_PIN, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(SETUP_PIN, GPIO_PULLUP_ONLY);
+    gpio_pullup_en(SETUP_PIN);
+    if (gpio_get_level(SETUP_PIN) == 0) {
+        setup = true; 
+    } 
+    gpio_reset_pin(SETUP_PIN);
+    // TODO start actual setup mode
+
+    init_hardware_timer();
 
     if (sender) {
-        int i;
-        esp_err_t ret;
-        size_t bytes_to_read = sizeof(i2sbuf);
-        size_t bytes_read = 0;
-        char t[][15] = {"", "ISR", "begin loop", "channel_read", "packing", "udp send", "end loop" }; 
-        int p = 0;
-        
         // initialize Wifi STA and UDP
         // enable_network_sender();
         
@@ -209,55 +278,22 @@ void app_main(void) {
         i2s_new_channel(&chan_cfg, NULL, &rx_handle);
         // this will later be i2s_channel_init_tdm_mode(). 
         i2s_channel_init_tdm_mode(rx_handle, &rx_cfg);
+
+        // create UDP sender task
+        xTaskCreate(udp_tx_task, "udp_tx_task", 4096, NULL, 5, &udp_send_task_handle);
+        
+        // create I2S rx on_recv callback
+        i2s_event_callbacks_t cbs = {
+            .on_recv = i2s_rx_callback,
+            .on_recv_q_ovf = NULL; 
+            .on_sent = NULL,
+            .on_send_q_ovf = NULL,
+        };
+        i2s_channel_register_event_callback(rx_handle, &cbs, NULL));
+
         // enable channel
         i2s_channel_enable(rx_handle);
-    
-        init_hardware_timer();
-
-        while (1) {
-            // gpio_set_level(SIG_PIN, 1);
-            ret = i2s_channel_read(rx_handle, i2sbuf, bytes_to_read, &bytes_read, 1000);
-            ESP_LOGI (TAG, "channel_read: %d bytes", bytes_read);
-
-            _log[p].loc = 3;
-            _log[p].time = get_time_us_in_isr();
-            p++;
-
-            // gpio_set_level(SIG_PIN, 0);
-            if (ret == ESP_OK && bytes_read == bytes_to_read) {
-                // packing
-/*                
-                for (i=0; i<NSAMPLES; ++i) {   
-                    memcpy(udpbuf+3*i, i2sbuf+4*i, 3); 
-                }
-
-                _log[p].loc = 4;
-                _log[p].time = get_time_us_in_isr();
-                p++;    
-*/
-                // UDP send            
-                // ESP_LOGI(TAG, "%s: triggering UDP send", __func__);
-                
-
-                _log[p].loc = 5;
-                _log[p].time = get_time_us_in_isr();
-                p++;
-
-                if (p > NUM) {
-                    int q = 0; 
-                    printf ("%15s %ld \n", t[_log[q].loc], _log[0].time);
-                    for (q=1; q<p; q++) {
-                        printf ("%15s %ld diff %ld\n", t[_log[q].loc], _log[q].time, _log[q].time - _log[q-1].time);
-                    }
-                    vTaskDelete(NULL); 
-                }
-
-
-            } else {
-                ESP_LOGE (TAG, "I2S read error: %d", ret);
-            }        
         
-        }
     } else {
         // initialize Wifi AP and UDP listener
         // enable_network_receiver();
@@ -273,7 +309,7 @@ void app_main(void) {
     }
     
     // we should never end up here. 
-    vTaskDelete(NULL);
+
 
 }
 

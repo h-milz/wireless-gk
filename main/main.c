987adceed831835f3551b35e0e88f64b4af5c33f
Harald Milz <hm@seneca.muc.de>
2025-01-07 17:54:27 +0100

    GPLv3 license information

--- main/main.c
+++ main/main.c
@@ -1,7 +1,21 @@
 /* 
- * simulate the receiving side UDP packet unpacking from 
- * 24 byte / sample to 32 byte / sample for AK4438 mode 18. 
- */ 
+    Copyright (C) 2024 Harald Milz <hm@seneca.muc.de> 
+
+    This file is part of Wireless-GK.
+    
+    Wireless-GK is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Wireless-GK.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "wireless_gk.h"
 

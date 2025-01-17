443934d2a58b7181716ec74d407d90502ddde569
Harald Milz <hm@seneca.muc.de>
2025-01-17 17:17:21 +0100

    ring counter

---
+++ tools/add.c
@@ -0,0 +1,49 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h> 
+#include <stdint.h>
+#include <sys/time.h>
+
+
+#define LOOPS 100000007
+#define NUM_UDP_BUFS 2
+
+struct timeval tv_start, tv_stop;
+int i, k;
+
+void main(void) {
+
+    k = 0;
+    gettimeofday(&tv_start,NULL);
+    for (i=0; i<LOOPS; i++) {
+        k = (k+1) % NUM_UDP_BUFS;
+    }
+    gettimeofday(&tv_stop,NULL);
+    printf ("variant 1: %lu µs for %d loops; k = %d\n", (tv_stop.tv_sec*(uint64_t)1000000+tv_stop.tv_usec - 
+                                                        (tv_start.tv_sec*(uint64_t)1000000+tv_start.tv_usec)), 
+                                                        LOOPS, 
+                                                        k);
+ 
+    k = 0;
+    gettimeofday(&tv_start,NULL);
+    for (i=0; i<LOOPS; i++) {
+        k = (k+1) >= NUM_UDP_BUFS ? 0 : k+1;
+    }
+    gettimeofday(&tv_stop,NULL);
+    printf ("variant 2: %lu µs for %d loops; k = %d\n", (tv_stop.tv_sec*(uint64_t)1000000+tv_stop.tv_usec - 
+                                                        (tv_start.tv_sec*(uint64_t)1000000+tv_start.tv_usec)), 
+                                                        LOOPS, 
+                                                        k);
+    k = 0;
+    gettimeofday(&tv_start,NULL);
+    for (i=0; i<LOOPS; i++) {
+        k = (k+1) & ~NUM_UDP_BUFS; 
+    }
+    gettimeofday(&tv_stop,NULL);
+    printf ("variant 3: %lu µs for %d loops; k = %d\n", (tv_stop.tv_sec*(uint64_t)1000000+tv_stop.tv_usec - 
+                                                        (tv_start.tv_sec*(uint64_t)1000000+tv_start.tv_usec)), 
+                                                        LOOPS, 
+                                                        k);
+    
+
+}

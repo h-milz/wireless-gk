f85e00f993ad27552bff9db2011ef4a0976f1b47
Harald Milz <hm@seneca.muc.de>
2025-01-17 17:24:00 +0100

    loop counter optimization

--- tools/add.c
+++ tools/add.c
@@ -6,7 +6,8 @@
 
 
 #define LOOPS 100000007
-#define NUM_UDP_BUFS 2
+#define NUM_UDP_BUFS 4
+#define NUM_UDP_BUFS_M_1 (NUM_UDP_BUFS-1)
 
 struct timeval tv_start, tv_stop;
 int i, k;
@@ -45,5 +46,17 @@ void main(void) {
                                                         LOOPS, 
                                                         k);
     
+    k = 0;
+    gettimeofday(&tv_start,NULL);
+    for (i=0; i<LOOPS; i++) {
+        k = (k+1) & NUM_UDP_BUFS_M_1; 
+    }
+    gettimeofday(&tv_stop,NULL);
+    printf ("variant 4: %lu µs for %d loops; k = %d\n", (tv_stop.tv_sec*(uint64_t)1000000+tv_stop.tv_usec - 
+                                                        (tv_start.tv_sec*(uint64_t)1000000+tv_start.tv_usec)), 
+                                                        LOOPS, 
+                                                        k);
+    
+
 
 }

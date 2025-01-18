20320eb6e47c44797eb90ecd1bf5f00ad8f29611
Harald Milz <hm@seneca.muc.de>
2025-01-18 11:32:43 +0100

    correction

--- main/wgk_sender.c
+++ main/wgk_sender.c
@@ -276,8 +276,8 @@ void i2s_rx_task(void *args) {
             }                    
         }
 #endif
-        // insert current packet count into the last slot. 
-        count++; 
+        // insert current packet count into the last slot. only 24 least significant bits
+        count = (count + 1) & 0x00FFFFFF; 
         memcpy ((uint32_t *)udpbuf[0] + (NUM_SLOTS_UDP-1) * SLOT_SIZE_UDP, 
                 &count,
                 sizeof(uint32_t)); 

7e9f8ed7bb1394e5678c39b0f54ee1ba1577441b
Harald Milz <hm@seneca.muc.de>
2025-02-07 16:45:32 +0100

    main/wgk_receiver.c: changing WIFI channel

--- main/wgk_receiver.c
+++ main/wgk_receiver.c
@@ -184,7 +184,7 @@ void init_wifi_rx(bool setup_requested) {
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
 #ifdef TX_TEST
-    int channel = 100;
+    int channel = WIFI_CHANNEL;
 #else    
     int channel = find_free_channel(); 
 #endif    
